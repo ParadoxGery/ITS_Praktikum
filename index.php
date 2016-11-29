4f7da65dc68f9e3727cda46e9af066b8a86e7d0c
Marc Kaepke <marc.kaepke@gmail.com>
2016-11-29 11:12:51 +0100

    foo

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include './web/index.php'; ?>
+<?php
+
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+Debug::enable();
+
+$app = require __DIR__.'/src/app.php';
+require __DIR__ . '/config/config.php';
+require __DIR__.'/src/controllers.php';
+$app->run();
