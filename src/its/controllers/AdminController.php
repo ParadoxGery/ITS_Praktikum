7b578a44afdc65a4fc0ac5574b9bc97592cdd433
Marc Kaepke <marc.kaepke@gmail.com>
2016-12-01 15:20:31 +0100

    user anlegen formular

--- src/its/controllers/AdminController.php
+++ src/its/controllers/AdminController.php
@@ -4,14 +4,48 @@ namespace its\controllers;
 use Silex\Api\ControllerProviderInterface;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AdminController implements ControllerProviderInterface{
 
     public function index(Application $app, Request $request){
         $users = $app['db']->fetchAll('SELECT * FROM users');
-		
+
+
+        // some default data for when the form is displayed the first time
+        $data = array(
+            'name' => 'name',
+            'email' => 'email',
+            'password' => 'password',
+        );
+
+        $form = $app['form.factory']->createBuilder(FormType::class, $data)
+            ->add('name')
+            ->add('email')
+            ->add('password')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            // do something with the data
+            $app['db']->insert('users', array(
+                'username' => $data['name'],
+                'mail' => $data['email'],
+                'password' => $data['password'],
+            ));
+
+            // redirect somewhere
+            return $app->redirect('/admin');
+        }
+
+
         return $app['twig']->render('admin/index.html.twig', array(
             'users' => $users,
+            'form' => $form->createView(),
         ));
     }
 
