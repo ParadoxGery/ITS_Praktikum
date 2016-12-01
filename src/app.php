5e7c90fb0d96d0a7629023966443c0c4a6c89f0a
gery <paradoxgery@gmx.net>
2016-12-01 15:23:53 +0100

    fix

--- src/app.php
+++ src/app.php
@@ -8,13 +8,14 @@ use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\LocaleServiceProvider();
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new FormServiceProvider());
-$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new LocaleServiceProvider());
 $app->register(new TranslationServiceProvider(), array(
 	'translator.domains' => array(),
 	'locale_fallbacks' => array('en'),
