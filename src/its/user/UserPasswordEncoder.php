e5b18a8b98e69109d518cd9c4e32d4e40e68345a
mkaepke <ehK-sx7-kmJ-otg>
2016-12-06 16:00:23 +0100

    foo

--- src/its/user/UserPasswordEncoder.php
+++ src/its/user/UserPasswordEncoder.php
@@ -13,11 +13,11 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserPasswordEncoder implements UserPasswordEncoderInterface
 {
-    public function encodePassword(UserInterface $user, string $plainpassword) {
+    public function encodePassword(UserInterface $user, $plainpassword) {
         return password_hash($plainpassword, PASSWORD_DEFAULT);
     }
 
-    public function isPasswordValid(UserInterface $user, string $raw) {
+    public function isPasswordValid(UserInterface $user, $raw) {
         return password_verify($raw, $user->getPassword());
     }
 }
